4bac7dd16d54da4477134d9c2b919cfac0186fad
Alexis 'Horgix' Chotard <alexis.horgix.chotard@gmail.com>
2017-01-03 20:47:47 +0100

    arch_chroot: fix arch_init path and chroot for real

--- arch_chroot.sh
+++ arch_chroot.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 
-apt-get install haveged
+apt-get install -y haveged
 systemctl start haveged
 cd /tmp
 curl -O https://mirrors.kernel.org/archlinux/iso/2016.12.01/archlinux-bootstrap-2016.12.01-x86_64.tar.gz
 tar xzf ./archlinux-bootstrap-2016.12.01-x86_64.tar.gz
 echo "$(awk 'c&&c--{sub(/^#/,"")} /## France/{c=8} 1' /tmp/root.x86_64/etc/pacman.d/mirrorlist)" > /tmp/root.x86_64/etc/pacman.d/mirrorlist
-curl -o /tmp/arch_init.sh https://raw.githubusercontent.com/Horgix/arch_chroot_bootstrap/master/arch_init.sh
-chmod +x /tmp/arch_init.sh
+curl -o /tmp/root.x86_64/arch_init.sh https://raw.githubusercontent.com/Horgix/arch_chroot_bootstrap/master/arch_init.sh
+chmod +x /tmp/root.x86_64/arch_init.sh
+/tmp/root.x86_64/bin/arch-chroot /tmp/root.x86_64/
