cd87c891c3f14f50cfafc734b483bbae9fc3c319
Alexis Horgix Chotard <alexis.horgix.chotard@gmail.com>
2017-06-05 01:56:25 +0200

    arch_chroot: update iso

--- arch_chroot.sh
+++ arch_chroot.sh
@@ -3,7 +3,7 @@
 apt-get install -y haveged
 systemctl start haveged
 cd /tmp
-curl -O https://mirrors.kernel.org/archlinux/iso/2016.12.01/archlinux-bootstrap-2016.12.01-x86_64.tar.gz
+curl -O http://mir.archlinux.fr/iso/2017.06.01/archlinux-bootstrap-2017.06.01-x86_64.tar.gz
 tar xzf ./archlinux-bootstrap-2016.12.01-x86_64.tar.gz
 echo "$(awk 'c&&c--{sub(/^#/,"")} /## France/{c=8} 1' /tmp/root.x86_64/etc/pacman.d/mirrorlist)" > /tmp/root.x86_64/etc/pacman.d/mirrorlist
 curl -o /tmp/root.x86_64/arch_init.sh https://raw.githubusercontent.com/Horgix/arch_chroot_bootstrap/master/arch_init.sh
