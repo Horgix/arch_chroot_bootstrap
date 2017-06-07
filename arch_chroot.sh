7598523e3e655d03df185a70bb69f21eaed66f7a
Alexis Horgix Chotard <alexis.horgix.chotard@gmail.com>
2017-06-07 16:34:23 +0200

    arch_chroot: bind /dev/sda for nspawn

--- arch_chroot.sh
+++ arch_chroot.sh
@@ -52,11 +52,11 @@ if [ "$1" == "--fullinstall" ]; then
   if [ ! -z $2 ]; then
     info "Found hostname $2 ; passing it to subsequent calls"
     info "Let's go for nspawn \o/"
-    systemd-nspawn -D /tmp/root.x86_64/ ./arch_init.sh $2
+    systemd-nspawn --bind /dev/sda -D /tmp/root.x86_64/ ./arch_init.sh $2
   else
     info "No hostname found"
     info "Let's go for nspawn \o/"
-    systemd-nspawn -D /tmp/root.x86_64/ ./arch_init.sh
+    systemd-nspawn --bind /dev/sda -D /tmp/root.x86_64/ ./arch_init.sh
   fi
 else
   info 'No full install, just chrooting you'
